063ca2f2ff251cf8a23a38febb34df6b568b25e0
Carlos <carlos@macs-mbp-2.attlocal.net>
2017-06-05 19:13:59 -0700

    first commit

---
+++ Syllabus/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Syllabus
+//
+//  Created by Carlos on 6/5/17.
+//  Copyright © 2017 Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    // Take Picture button
+    @IBAction func takePicture(_ sender: Any) {
+        
+    }
+    
+    // Image view
+    @IBOutlet var image: UIImageView!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
